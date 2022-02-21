7a1f9eef17fd98456048e64bd3cad3c3fe6ba304
KillerJumper <82290559+KillerJumper@users.noreply.github.com>
2022-02-21 17:30:54 +0100

    Create index.js

---
+++ handler/index.js
@@ -0,0 +1,47 @@
+const { glob } = require("glob");
+const { promisify } = require("util");
+const { Client } = require("discord.js");
+
+const globPromise = promisify(glob);
+
+/**
+ * @param {Client} bot
+ */
+module.exports = async (bot) => {
+    // Commands
+    const commands = await globPromise(`${process.cwd()}/commands/*/*.js`);
+    commands.map((value) => {
+        const file = require(value);
+        const splitted = value.split("/");
+        const directory = splitted[splitted.length - 2];
+
+        if (file.name) {
+            const properties = { directory, ...file };
+            bot.commands.set(file.name, properties);
+        }
+    });
+
+    // Events
+    const eventFiles = await globPromise(`${process.cwd()}/events/*/*.js`);
+    eventFiles.map((value) => require(value));
+
+    // Slash Commands
+    const slashCommands = await globPromise(
+        `${process.cwd()}/SlashCommands/*/*.js`
+    );
+
+    const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+        const file = require(value);
+        if (!file?.name) return;
+        bot.slashCommands.set(file.name, file);
+
+        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+        arrayOfSlashCommands.push(file);
+    });
+
+    bot.on("ready", async () => {
+        await bot.application.commands
+        .set(arrayOfSlashCommands);
+    });
+};
