979c7743fd8ed25f19bd93aebe4bbf5ec3be78d0
KillerJumper <82290559+KillerJumper@users.noreply.github.com>
2022-02-21 17:49:02 +0100

    Create music.js

---
+++ SlashCommands/music.js
@@ -0,0 +1,16 @@
+const {Client, CommandInteraction, MessageEmbed} = require('discord.js');
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: "clash",
+    description: "Affiche un clash aléatoire !",
+    type: 'CHAT_INPUT',
+    /**
+     *
+     * @param {Client} bot
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (bot, interaction, args) =>{
+    }
+}
